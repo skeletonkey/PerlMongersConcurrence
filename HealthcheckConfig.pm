7bac7e1121cb2e6b62ba4cdabaa05e86bbf97d10
Erik Tank <erik.tank@ticketmaster.com>
2020-04-26 10:17:26 -0700

    HealthcheckConfig updates for k8s
    Create a discovery for pods in k8s and allow for multiple executions
    in one process.

--- HealthcheckConfig.pm
+++ HealthcheckConfig.pm
@@ -3,20 +3,46 @@ package HealthcheckConfig;
 use strict;
 use warnings;
 
-my @start_time;
-BEGIN { @start_time = localtime(); }
-sub print_execution_time  {
+sub get_execution_time {
+    my $self = shift;
+
+    my @start_time = @{$self->{start_time}};
     my @end_time = localtime();
-    my $runtime = ($end_time[2] - $start_time[2]) * 3600 + ($end_time[1] - $start_time[1]) * 60 + ($end_time[0] - $start_time[0]);
+    return ($end_time[2] - $start_time[2]) * 3600 + ($end_time[1] - $start_time[1]) * 60 + ($end_time[0] - $start_time[0]);
+}
+
+sub print_execution_time  {
+    my $self = shift;
+    my $runtime = $self->get_execution_time();
     print "\nScript Execution time: $runtime seconds\n";
 }
 
-sub new { return bless {}, __PACKAGE__; }
+sub new { return bless { start_time => [localtime()], }, __PACKAGE__; }
 
-sub healthcheck_col_count    { return 4;                                                        }
-sub healthcheck_uri_template { return 'http://localhost:%d/healthcheck';                        }
-sub nodes                    { return 100;                                                      }
-sub port_start               { return 5000;                                                     }
+sub healthcheck_col_count    { return $ENV{HEALTHCHECK_COLUMN_COUNT} || 2; }
+sub healthcheck_uri_template { return 'http://%s/healthcheck'; }
+
+sub nodes {
+    my @lines = `microk8s kubectl describe pod --namespace default | grep IP:`;
+    warn(sprintf("Seems that not all nodes are up yet - found: %d, but looking for %d\n", scalar(@lines), node_count() * 2)) unless scalar(@lines) == node_count() * 2;
+
+    my %ips = ();
+    foreach my $line (@lines) {
+        my ($ip) = $line =~ /IP:\s*(\S+)/;
+        next unless $ip;
+        $ips{$ip} = 1;
+    }
+
+    return keys %ips;
+}
+
+sub node_count {
+    my @lines = `microk8s kubectl describe deployment mock-healthcheck-deployment | grep replicas`;
+    my ($node_count) = $lines[0] =~ /\((\d+)\/\d+ replicas created/;
+    $node_count ||= 0;
+    die("Seemed that I didn't find a node_count ($node_count) from $lines[0]\n") unless $node_count;
+    return $node_count;
+}
 
 sub output_file {
     my $self = shift;
@@ -24,16 +50,16 @@ sub output_file {
     return "$ENV{PMP_BASE_DIR}/healthcheck_report/${name}healthcheck.html";
 }
 
-sub max_curl_timeout         { return 10;                                                       }
-sub tmp_dir                  { return '/tmp';                                                   }
+sub max_curl_timeout         { return $ENV{CURL_MAX_TIMEOUT} || 10; }
+sub tmp_dir                  { return '/tmp';                       }
 
 sub html_top_template {
     return q+
 <html>
     <head>
         <title>%s</title>
-        <meta http-equiv="refresh" content="65">
-        <style>* { font-size: 18px; font-family: Arial; } table, th, td { border: 1px solid #c4c4c4; border-collapse: collapse; } th, td { padding: 2px; text-align: center; } td { font-family: courier; } .warning { background: red; color: white }</style>
+        <meta http-equiv="refresh" content="5">
+        <style>* { font-size: 18px; font-family: Arial; } table, th, td { border: 1px solid #c4c4c4; border-collapse: collapse; } th, td { padding: 2px; text-align: left; } td { font-family: courier; } .warning { background: red; color: white }</style>
     </head>
     <body>
         Last Updated: <span id="report_time"></span>
